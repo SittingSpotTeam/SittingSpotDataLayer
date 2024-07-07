9fa38143ad30504642703d92819e9e37662c421d
Leonardo Malcotti <leo@Leonardos-Air.lan>
2024-07-07 14:31:11 +0200

    initialized spring app

---
+++ src/main/java/com/sittingspot/sittingspotdatalayer/SittingspotdatalayerApplication.java
@@ -0,0 +1,13 @@
+package com.sittingspot.sittingspotdatalayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SittingspotdatalayerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SittingspotdatalayerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sittingspot/sittingspotdatalayer/SittingspotdatalayerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sittingspot.sittingspotdatalayer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SittingspotdatalayerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
